3eb1ae82970f6846b5280137d8d7546b66c10305
Reinid <reinidval@gmail.com>
2024-06-18 20:02:18 -0300

    finish exercises level 2 body: commenting all the exercises

--- 5th_day.py
+++ 5th_day.py
@@ -109,9 +109,267 @@ front_end.extend(back_end)
 print(front_end)
 
 ## 27. After joining the lists in question 26. Copy the joined list and assign it to a variable full_stack. Then insert Python and SQL after Redux.
-full_stack = front_end.copy()
+"""full_stack = front_end.copy()
 print(full_stack.count("Redux"))
 print(full_stack.index("Redux"))
 full_stack.insert(full_stack.index("Redux") + 1, 'Python')
 full_stack.insert(full_stack.index("Python") + 1, 'SQL')
 print(full_stack)
+"""
+# Exercises level 2: fifth day
+## Given the age list...
+
+##
+##- 8. Divide the countries list into two equal lists if it is even if not one more country for the first half.
+##- 9. ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']. Unpack the first three countries and the rest as scandic countries.
+
+## 1. Sort the list and find the min and max age
+"""
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+ages.sort()
+
+min_age = ages[0]
+max_age = ages[-1]
+print(f"1. mininum age is {min_age}, and maximum age is {max_age}")
+
+min_age = min(ages)
+max_age = max(ages)
+print(f"1. mininum age is {min_age}, and maximum age is {max_age}")
+
+## 2. Add the min age and the max age again to the list
+ages.insert(0, min_age)
+ages.append(max_age)
+
+## 3. Find the median age (one middle item or two middle items divided by two)
+print(ages)
+print(len(ages))
+middle_index =  (len(ages) - 1) // 2
+print('The median age is: {0}'.format(ages[middle_index]))
+
+##-4. Find the average age (sum of all items divided by their number )
+age_average = (sum(ages) / len(ages))
+print('Knowing that there are {quantity} elements and the sum is {total}, the age average is: {average}'.format(quantity = len(ages), total = sum(ages), average = age_average))
+
+##-5. Find the range of the ages (max minus min)
+age_range = max(ages) - min(ages)
+print(f'The age range is: {age_range}, from {min(ages)} to {max(ages)}')
+
+##-6. Compare the value of (min - average) and (max - average), use abs() method
+value_1 = abs(min(ages) - age_average)
+value_2 = abs(max(ages) - age_average)
+evaluation = value_1 == value_2
+print(f'value 1 is {value_1}, value 2 is: {value_2} and it is {evaluation} that they are equals')
+"""
+##- 7. Find the middle country(ies) in the countries list
+"""
+countries = [
+  'Afghanistan',
+  'Albania',
+  'Algeria',
+  'Andorra',
+  'Angola',
+  'Antigua and Barbuda',
+  'Argentina',
+  'Armenia',
+  'Australia',
+  'Austria',
+  'Azerbaijan',
+  'Bahamas',
+  'Bahrain',
+  'Bangladesh',
+  'Barbados',
+  'Belarus',
+  'Belgium',
+  'Belize',
+  'Benin',
+  'Bhutan',
+  'Bolivia',
+  'Bosnia and Herzegovina',
+  'Botswana',
+  'Brazil',
+  'Brunei',
+  'Bulgaria',
+  'Burkina Faso',
+  'Burundi',
+  'Cambodia',
+  'Cameroon',
+  'Canada',
+  'Cape Verde',
+  'Central African Republic',
+  'Chad',
+  'Chile',
+  'China',
+  'Colombi',
+  'Comoros',
+  'Congo (Brazzaville)',
+  'Congo',
+  'Costa Rica',
+  "Cote d'Ivoire",
+  'Croatia',
+  'Cuba',
+  'Cyprus',
+  'Czech Republic',
+  'Denmark',
+  'Djibouti',
+  'Dominica',
+  'Dominican Republic',
+  'East Timor (Timor Timur)',
+  'Ecuador',
+  'Egypt',
+  'El Salvador',
+  'Equatorial Guinea',
+  'Eritrea',
+  'Estonia',
+  'Ethiopia',
+  'Fiji',
+  'Finland',
+  'France',
+  'Gabon',
+  'Gambia, The',
+  'Georgia',
+  'Germany',
+  'Ghana',
+  'Greece',
+  'Grenada',
+  'Guatemala',
+  'Guinea',
+  'Guinea-Bissau',
+  'Guyana',
+  'Haiti',
+  'Honduras',
+  'Hungary',
+  'Iceland',
+  'India',
+  'Indonesia',
+  'Iran',
+  'Iraq',
+  'Ireland',
+  'Israel',
+  'Italy',
+  'Jamaica',
+  'Japan',
+  'Jordan',
+  'Kazakhstan',
+  'Kenya',
+  'Kiribati',
+  'Korea, North',
+  'Korea, South',
+  'Kuwait',
+  'Kyrgyzstan',
+  'Laos',
+  'Latvia',
+  'Lebanon',
+  'Lesotho',
+  'Liberia',
+  'Libya',
+  'Liechtenstein',
+  'Lithuania',
+  'Luxembourg',
+  'Macedonia',
+  'Madagascar',
+  'Malawi',
+  'Malaysia',
+  'Maldives',
+  'Mali',
+  'Malta',
+  'Marshall Islands',
+  'Mauritania',
+  'Mauritius',
+  'Mexico',
+  'Micronesia',
+  'Moldova',
+  'Monaco',
+  'Mongolia',
+  'Morocco',
+  'Mozambique',
+  'Myanmar',
+  'Namibia',
+  'Nauru',
+  'Nepal',
+  'Netherlands',
+  'New Zealand',
+  'Nicaragua',
+  'Niger',
+  'Nigeria',
+  'Norway',
+  'Oman',
+  'Pakistan',
+  'Palau',
+  'Panama',
+  'Papua New Guinea',
+  'Paraguay',
+  'Peru',
+  'Philippines',
+  'Poland',
+  'Portugal',
+  'Qatar',
+  'Romania',
+  'Russia',
+  'Rwanda',
+  'Saint Kitts and Nevis',
+  'Saint Lucia',
+  'Saint Vincent',
+  'Samoa',
+  'San Marino',
+  'Sao Tome and Principe',
+  'Saudi Arabia',
+  'Senegal',
+  'Serbia and Montenegro',
+  'Seychelles',
+  'Sierra Leone',
+  'Singapore',
+  'Slovakia',
+  'Slovenia',
+  'Solomon Islands',
+  'Somalia',
+  'South Africa',
+  'Spain',
+  'Sri Lanka',
+  'Sudan',
+  'Suriname',
+  'Swaziland',
+  'Sweden',
+  'Switzerland',
+  'Syria',
+  'Taiwan',
+  'Tajikistan',
+  'Tanzania',
+  'Thailand',
+  'Togo',
+  'Tonga',
+  'Trinidad and Tobago',
+  'Tunisia',
+  'Turkey',
+  'Turkmenistan',
+  'Tuvalu',
+  'Uganda',
+  'Ukraine',
+  'United Arab Emirates',
+  'United Kingdom',
+  'United States',
+  'Uruguay',
+  'Uzbekistan',
+  'Vanuatu',
+  'Vatican City',
+  'Venezuela',
+  'Vietnam',
+  'Yemen',
+  'Zambia',
+  'Zimbabwe',
+]
+
+if len(countries) % 2 != 0:
+    middle_index = len(countries) // 2
+    middle_country = countries[middle_index]
+    print(middle_country)
+else:
+    middle_countries = [countries[len(countries) // 2],countries[(len(countries) // 2) + 1]]
+    print(f'{middle_countries}')
+"""
+# ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']. Unpack the first three countries and the rest as scandic countries.
+"""
+countries_to_unpack = ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']
+modern_empire_1, modern_empire_2, modern_empire_3, *scandic = countries_to_unpack
+
+print(modern_empire_1, modern_empire_2, modern_empire_3, scandic)
+"""
